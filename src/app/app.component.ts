6928bf8753a61cfd9a4fd96bf500101d83efa8dd
Kevin <kevinadler12345@gmail.com>
2023-01-16 11:41:14 +0100

    grid layout customized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { _getEventTarget } from '@angular/cdk/platform';
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
+  makeMove() {
+    alert();
+  }
+
+  wins = [
+    [0, 1, 2], /* sekrecht*/
+    [3, 4, 5],
+    [6, 7, 8],
+
+    [0, 3, 6], /* waagrecht*/
+    [1, 4, 7],
+    [2, 5, 8],
+
+    [0, 4, 8], /* diagonal*/
+    [2, 4, 6]
+  ]
 
 }
